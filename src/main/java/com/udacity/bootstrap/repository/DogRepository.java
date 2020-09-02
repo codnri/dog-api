3df7ee28d884cb74e9929c861a459959a687a183
coddy <izo.kodak@gmail.com>
2020-09-02 19:55:57 -0400

    modified sql file name

--- src/main/java/com/udacity/bootstrap/repository/DogRepository.java
+++ src/main/java/com/udacity/bootstrap/repository/DogRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface DogRepository extends CrudRepository<Dog, Long> {
-
     @Query("select d.id, d.breed from Dog d where d.id=:id")
     String findBreedById(Long id);
 
@@ -16,15 +15,4 @@ public interface DogRepository extends CrudRepository<Dog, Long> {
 
     @Query("select d.id, d.name from Dog d")
     List<String> findAllNames();
-    /*
-    @Query(value="SELECT breed FROM Dog",nativeQuery = true)
-    public List<String> findAllBreeds();
-
-    @Query(value="SELECT breed FROM Dog WHERE id=:Id",nativeQuery = true)
-    public String findBreedById(Long Id);
-
-    @Query(value="SELECT name FROM Dog",nativeQuery = true)
-    public List<String> findAllNames();
-
-     */
 }
